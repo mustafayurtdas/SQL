4c80f3667e8913f940576200bba777287ff19760
Mustafa Yurtdas <myurtdas06@gmail.com>
2023-03-08 23:55:04 +0300

    day07

--- day07 EXISTS.sql
+++ day07 EXISTS.sql
@@ -35,12 +35,17 @@ INSERT INTO nisan VALUES (50, 'Yasar', 'Volvo');
 --MART ve NİSAN aylarında aynı urun_id ile satılan ürünlerin urun_id'lerini
   ve aynı zamanda bu ürünleri MART ayında alan musteri_isim 'lerini listeleyen bir sorgu yazınız. 
 */
+SELECT urun_id,musteri_isim FROM mart 
+WHERE EXISTS (SELECT urun_id FROM nisan WHERE mart.urun_id = nisan.urun_id);
 
 
 /*
 --MART ve NİSAN aylarında aynı urun_isim'i ile satılan ürünlerin urun_isim'lerini                  
 ve aynı zamanda bu ürünleri NİSAN ayında alan musteri_isim'lerini listeleyen bir sorgu yazınız.
 */
+
+SELECT urun_isim, musteri_isim FROM nisan 
+WHERE EXISTS (SELECT urun_isim FROM mart WHERE mart.urun_isim = nisan.urun_isim);
 
 /*ÖDEV:
 
@@ -49,8 +54,9 @@ ve aynı zamanda bu ürünleri NİSAN ayında alan musteri_isim'lerini listeleye
 */
 
 
+select * from nisan;
 
-
+select * from mart;
 
 
 
--- day08 JOIN.sql
+++ day08 JOIN.sql
@@ -60,31 +60,107 @@ insert into developer_dersler values (108, 'Micro Services', 12);
 --SORU1: join kullanarak qa_dersler'ini listeleyeniz
 
 
-select A.ders_id, A.ders_ismi, A.ders_saati from qa_dersler as A
-left join developer_dersler as B
-on A.ders_id=B.ders_id;
+SELECT A.ders_id, A.ders_ismi, A.ders_saati FROM qa_dersler as A
+lEFT JOIN developer_dersler as B
+ON A.ders_id = B.ders_id;
 
 -- 2.YOL
 
-select A.ders_id, A.ders_ismi, A.ders_saati from developer_dersler as B
-right join qa_dersler as A 
-on A.ders_id=B.ders_id;
+SELECT A.ders_id, A.ders_ismi, A.ders_saati FROM developer_dersler as B
+RIGHT JOIN qa_dersler AS A 
+ON A.ders_id = B.ders_id;
 
 
---SORU2: join kullanarak developer_dersler'ini listeleyiniz
+--SORU1: join kullanarak developer_dersler'ini listeleyiniz
+
+
+SELECT A.ders_id, A.ders_ismi, A.ders_saati FROM developer_dersler  AS A
+lEFT JOIN qa_dersler AS B
+ON A.ders_id = B.ders_id;
+
+-- 2.YOL
+
+SELECT B.ders_id, B.ders_ismi, B.ders_saati FROM qa_dersler AS A
+RIGHT JOIN developer_dersler AS B 
+ON A.ders_id = B.ders_id;
+
+--SORU2: join kullanarak ortak dersleri listeleyiniz 
+
+SELECT A.ders_id, A.ders_ismi, A.ders_saati FROM qa_dersler AS A
+INNER JOIN developer_dersler AS B
+ON A.ders_id = B.ders_id;
+
+
+--SORU3: join kullanarak qa_dersleri ve developer_dersleri tablosundaki verilerin tamamını listeleyiniz.
+
+SELECT A.ders_id, A.ders_ismi, A.ders_saati FROM qa_dersler as A
+lEFT JOIN developer_dersler as B
+ON A.ders_id = B.ders_id
+
+UNION
+
+SELECT B.ders_id, B.ders_ismi, B.ders_saati FROM qa_dersler AS A
+RIGHT JOIN developer_dersler AS B 
+ON A.ders_id = B.ders_id;
 
 
 
+create table filmler
+(film_id int,
+film_name varchar(30),
+category varchar(30)
+);
+
+insert into filmler values (100, 'Eyvah Eyvah', 'Komedi');
+insert into filmler values (200, 'Kurtlar Vadisi', 'Aksiyon');
+insert into filmler values (300, 'Eltilerin Savasi', 'Komedi');
+insert into filmler values (400, 'Aile Arasinda', 'Komedi');
+insert into filmler values (500, 'GORA', 'Bilim Kurgu');
+insert into filmler values (600, 'Organize Isler', 'Komedi');
+insert into filmler values (700, 'Babam ve Oglum', 'Dram');
+
+
+create table aktorler
+(id int,
+actor_name varchar(30),
+film_id int
+);
+
+insert into aktorler values (1, 'Ata Demirer', 100);
+insert into aktorler values (2, 'Necati Sasmaz', 200);
+insert into aktorler values (3, 'Gupse Ozay', 300);
+insert into aktorler values (4, 'Engin Gunaydin', 400);
+insert into aktorler values (5, 'Cem Yilmaz', 500);
+
+
+select * from filmler;
+
+
+-- SORU1: Tüm film_name'leri, tüm category'lerini ve filmlerde oynayan actor_name'leri listeleyiniz.
+
+
+select film_name, category, actor_name from filmler as A
+left join aktorler as B
+on A.film_id=B.film_id
+
+--2.YOL
+select film_name, category, actor_name from aktorler as B
+right join filmler as A
+on A.film_id=B.film_id
 
 
 
+-- SORU2: Tüm actor_name'leri ve film_name'lerini listeleyiniz.
 
 
+select actor_name,film_name from aktorler as A
+left join filmler as B
+on A.film_id=B.film_id
 
-
-
-
-
+--2.YOL
+select actor_name,film_name from filmler as B
+right join aktorler as A
+on A.film_id=B.film_id
 
 
 
